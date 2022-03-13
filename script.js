881eb8d2b8a341b68f5bfe500bb91b83a1cfa358
Daniel Sam <daniellevilucas@gmail.com>
2022-03-14 00:02:26 +0530

    calculate() added

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const display = document.querySelector(".display");
 buttons.forEach((button) => {
   button.addEventListener("click", calculate);
 });
+
+function calculate(event) {
+  const clickedButtonValue = event.target.value;
+  try {
+    if (clickedButtonValue === "C") return (display.value = "");
+
+    if (clickedButtonValue === "=" && display.value !== "")
+      return (display.value = eval(display.value));
+
+    display.value = `${display.value} ${clickedButtonValue}`;
+  } catch (error) {
+    display.value = "invalid syntax";
+  }
+}
